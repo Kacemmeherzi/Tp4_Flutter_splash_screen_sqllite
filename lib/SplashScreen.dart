0ab785e4187dbff1dd37e020678ea6c174e8c47d
kacem <kacemmeherzi52@gmail.com>
2024-10-09 10:52:45 +0100

    splash screen updated

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,10 +1,38 @@
-import 'package:flutter/material.dart';
 
-class Splashscreen extends StatelessWidget {
-  const Splashscreen({super.key});
+import 'dart:async';
+
+import 'package:app/loginview.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => Loginview()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,  
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/loading_screen_animation.json', 
+          width: 200,
+          height: 200,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }

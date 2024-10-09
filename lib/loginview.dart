1f132b147c369e8bc43907cdbb6775d72d727f1b
kacem <kacemmeherzi52@gmail.com>
2024-10-09 11:52:24 +0100

    Form added with two form fields

--- lib/loginview.dart
+++ lib/loginview.dart
@@ -8,10 +8,76 @@ class Loginview extends StatefulWidget {
 }
 
 class _LoginviewState extends State<Loginview> {
+    final _formKey = GlobalKey<FormState>();
+ final TextEditingController  _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar:  AppBar(title: const  Text("Log in"),backgroundColor:Colors.green,),
+      body: Column(
+        children: [
+           Center(child: Image.asset("assets/images/logo.png"),heightFactor: 50,),
+     const      Text("Welcome Back ",style: TextStyle(color: Colors.green,fontSize: 20),),
+       const    Text("please sign in into your account  ",style: TextStyle(color: Colors.grey,fontSize: 10),),
+
+           Center(
+            child:Form( key: _formKey,
+           child: Column(
+            
+            children: [
+            Padding(padding: EdgeInsets.all(20),child: 
+
+             TextFormField(
+                controller: _emailController,
+                decoration:  const InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  
+                  labelText: 'email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              )),
+                          Padding(padding: EdgeInsets.all(20),child: 
+
+              TextFormField(
+                controller: _passwordController,
+                decoration: const  InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,  
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),),
+              ElevatedButton(style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green
+              ),
+                onPressed: (){}, child: const  Text("Sign in ")) , TextButton(child:const Text("Dont had one ?")
+            ,onPressed: (){
+                    Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Loginview()));
+            },)
+           ],),
+             
+            ), 
+             )
+
+
+
+        ],
+      ),
     );
 
   }

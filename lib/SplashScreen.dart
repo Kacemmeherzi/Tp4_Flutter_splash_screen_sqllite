ebe7d587e593d87276b4910fddba2f09e3ab0b26
kacem <kacemmeherzi52@gmail.com>
2024-11-01 13:55:35 +0100

    fixed

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -15,9 +17,9 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(Duration(seconds: 4), () {
+    Timer(const Duration(seconds: 4), () {
       Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => Loginview()));
+          MaterialPageRoute(builder: (context) => const Loginview()));
     });
   }
 
